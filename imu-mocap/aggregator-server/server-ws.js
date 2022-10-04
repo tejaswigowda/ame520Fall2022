6b8ad14847c47ca73263b0e83cba45d2c8a0eb07
Tejaswi Gowda <tejaswi@asu.edu>
2022-10-03 19:55:14 -0700

    ac

--- imu-mocap/aggregator-server/server-ws.js
+++ imu-mocap/aggregator-server/server-ws.js
@@ -1,8 +1,5 @@
 var express = require("express");
 var app = express();
-var bodyParser = require('body-parser');
-var errorHandler = require('errorhandler');
-var methodOverride = require('method-override');
 var hostname = process.env.HOSTNAME || 'localhost';
 const WebSocket = require('ws')
 var values = {}
@@ -21,11 +18,7 @@ wss.on('connection', ws => {
   ws.send('start');
 })
 
-
-app.use(methodOverride());
-app.use(bodyParser());
 app.use(express.static(__dirname + '/public'));
-app.use(errorHandler());
 
 console.log("Simple static server listening at http://" + hostname + ":1234" );
 app.listen(1234);
